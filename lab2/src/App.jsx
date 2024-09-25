e41ca5329fc873e59d7e7b2b9fbda59b472694f2
Paras <parasrawat0418@gmail.com>
2024-09-24 21:37:39 -0400

    LAb 2 is here

--- lab2/src/App.jsx
+++ lab2/src/App.jsx
@@ -7,53 +7,46 @@ function App() {
       <h1 style={{ color: "#8a959f" }}>Resorts Life</h1>
       <div className="country">
         <Card
-          country="Indonesia  "
-          name="Gill Air Hotel"
+          country="Indonesia"
+          name="Gili Air Hotel"
           image="src/assets/1.jpg"
           price="$589/night"
           rating="4.8 ★"
         />
         <Card
-          country="Seychelles  "
-          name="Seychelles"
+          country="Seychelles"
+          name="Hilton Resort"
           image="src/assets/2.jpg"
           price="$692/night"
           rating="4.2 ★"
         />
         <Card
           country="Us Virgin Island"
-          name="US Virgin Island"
+          name="Goa Resort"
           image="src/assets/3.jpg"
           price="$485/night"
           rating="3.5 ★ "
         />
         <Card
-          country="Bahamas  "
-          name="Bahamas"
+          country="Bahamas"
+          name="Kuredu Resort"
           image="src/assets/4.jpg"
           price="$729/night"
           rating="4.1 ★"
         />
         <Card
-          country="Mauritius  "
-          name="Mauritius"
+          country="Mauritius"
+          name="Trou D'eau Douce"
           image="src/assets/5.jpg"
           price="$877/night"
           rating="4.9★ "
         />
         <Card
-          country="Bermuda  "
-          name="Bermuda"
+          country="Bermuda"
+          name="Staniel Cay Hotel"
           image="src/assets/6.jpg"
           price="$365/night"
           rating="3.2 ★"
-        />
-        <Card
-          country="Los Santos "
-          name="Los Santos"
-          image="src/assets/7.jpg"
-          price="$420/night"
-          rating="4.2 ★"
         />
       </div>
     </>
